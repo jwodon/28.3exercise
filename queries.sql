85f70620fd13cff4d2bd604104d3e824c200cd4c
jwodon <146784748+jwodon@users.noreply.github.com>
2024-01-04 12:08:07 -0800

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,16 @@
+SELECT * FROM owners 
+    FULL OUTER JOIN vehicles 
+        ON owners.id = vehicles.owner_id;
+
+
+SELECT first_name, last_name, COUNT(owner_id) FROM owners
+    JOIN vehicles ON owners.id = vehicles.owner_id
+    GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+SELECT first_name, last_name, AVG(price) AS average_price, COUNT(owner_id) AS count FROM owners
+    JOIN vehicles ON owners.id = vehicles.owner_id
+    GROUP BY (first_name, last_name)
+    HAVING AVG(price) > 10000 AND COUNT(owner_id) > 1
+ORDER BY first_name DESC;
+
